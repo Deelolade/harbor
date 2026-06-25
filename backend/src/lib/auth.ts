19eb51b3ccd4ded96ee04a73b8224e0a83aa3bf5
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-06-25 09:58:35 +0100

    chore: better-auth, prisma setup complete

--- backend/src/lib/auth.ts
+++ backend/src/lib/auth.ts
@@ -1,7 +1,17 @@
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "@better-auth/prisma-adapter";
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { DATABASE_URL } from "../utils/env.js";
+
+const prisma = new PrismaClient({
+  adapter: new PrismaPg({ connectionString: DATABASE_URL }),
+});
 
 export const auth = betterAuth({
-  database: prismaAdapter(),
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
   baseURL: "http://localhost:8800/",
-  emailAndPassword: { enabled: true },
+  emailAndPassword: { enabled: true, autoSignIn: false },
 });
--- backend/src/utils/env.ts
+++ backend/src/utils/env.ts
@@ -1,1 +1,2 @@
 export const COOKIE_SECRET = process.env.COOKIE_SECRET;
+export const DATABASE_URL = process.env.DATABASE_URL;
