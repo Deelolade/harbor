05ce73b0ce2a25cd78251465cd7282cab52458ac
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-07-29 22:49:19 +0100

    update: worked on frontend build error

--- frontend/src/pages/overview/HomeView.tsx
+++ frontend/src/pages/overview/HomeView.tsx
@@ -132,7 +132,7 @@ export default function HomeView() {
     name: "",
     description: "",
     image: "",
-    visibility: "WORKSPACE" as const,
+    visibility: "WORKSPACE",
   });
 
   const createMutation = useMutation({
